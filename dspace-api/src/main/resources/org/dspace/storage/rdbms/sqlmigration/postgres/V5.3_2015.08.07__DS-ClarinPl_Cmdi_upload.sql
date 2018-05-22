c418e9cd651a097b0ad7f372c6683f992485a54f
Wojciech Rauk <wojtek.rauk@gmail.com>
2018-05-22 12:42:29 +0200

    all webapps working with docker

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.3_2015.08.07__DS-ClarinPl_Cmdi_upload.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.3_2015.08.07__DS-ClarinPl_Cmdi_upload.sql
@@ -2,4 +2,4 @@
 -- Name: cmdi_id
 --    reference bitstream id for connected bitstream with cmdi file
 --
--- ALTER TABLE bitstream ADD COLUMN cmdi_id integer default 0;
+ALTER TABLE bitstream ADD COLUMN cmdi_id integer default 0;
