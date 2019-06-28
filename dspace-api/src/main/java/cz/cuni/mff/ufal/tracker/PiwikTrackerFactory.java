df3a82e085d7d499ee52a93a8a5258a45e4c5b0b
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2019-06-28 11:54:27 +0200

    Null collection caused NPE
    Downloads from review step were impossible as the owning collection is null

--- dspace-api/src/main/java/cz/cuni/mff/ufal/tracker/PiwikTrackerFactory.java
+++ dspace-api/src/main/java/cz/cuni/mff/ufal/tracker/PiwikTrackerFactory.java
@@ -32,7 +32,14 @@ public class PiwikTrackerFactory
 
     public static Tracker createBitstreamTrackerInstance(Collection col){
         int defaultSiteId = configurationService.getPropertyAsType("lr.lr.tracker.bitstream.site_id", 0);
-        boolean disabled = configurationService.getPropertyAsType("lr.lr.tracker.collection."
+
+        if(col == null){
+            //There might be no collection for example if we try downloading as editors during review; the item has
+            // not been installed in any collection yet.
+            return new PiwikBitstreamTracker(defaultSiteId);
+        }
+
+        boolean disabled =  configurationService.getPropertyAsType("lr.lr.tracker.collection."
                 + col.getHandle() + ".disabled", false);
         if(disabled){
             return noopTracker;
