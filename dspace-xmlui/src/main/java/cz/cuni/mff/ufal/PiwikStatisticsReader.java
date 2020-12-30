a264c6116e668df7fb542915a701c99ba556e367
Ondřej Košarko <ko_ok@centrum.cz>
2020-12-30 21:53:09 +0100

    Refactoring
    removes unreachable code; more readable structure; no changes in
    functionality

--- dspace-xmlui/src/main/java/cz/cuni/mff/ufal/PiwikStatisticsReader.java
+++ dspace-xmlui/src/main/java/cz/cuni/mff/ufal/PiwikStatisticsReader.java
@@ -1,30 +1,13 @@
 package cz.cuni.mff.ufal;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.net.URL;
+import java.io.*;
 import java.net.URLEncoder;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
 
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.ProcessingException;
@@ -37,21 +20,12 @@ import org.apache.cocoon.reading.AbstractReader;
 import org.apache.log4j.Logger;
 import org.dspace.app.xmlui.utils.ContextUtil;
 import org.dspace.authorize.AuthorizeException;
-import org.dspace.authorize.AuthorizeManager;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
 import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
 import org.dspace.eperson.EPerson;
 import org.dspace.handle.HandleManager;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class PiwikStatisticsReader extends AbstractReader {
@@ -105,7 +79,7 @@ public class PiwikStatisticsReader extends AbstractReader {
             this.isSpider = par.getParameter("userAgent", "").equals("spider");
             
          	// Reference by an item's handle.
-            DSpaceObject dso = dso = HandleManager.resolveToObject(context, handle);
+            DSpaceObject dso = HandleManager.resolveToObject(context, handle);
 
             if (dso instanceof Item) {
                 item = (Item)dso;                
@@ -131,116 +105,160 @@ public class PiwikStatisticsReader extends AbstractReader {
 
 
 	@Override
-	public void generate() throws IOException, SAXException, ProcessingException {
-		
+	public void generate() throws ProcessingException {
 		try {
-			
-			
 			String mergedResult = "";
-			
-			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");			
-			
-			// should contain the period
-			String queryString = request.getQueryString();									
-			String period = request.getParameter("period");
-			
-			
 			if(PIWIK_API_MODE==null || PIWIK_API_MODE.equals("direct")) {
-				
-				Calendar cal = Calendar.getInstance();
-				// default start and end data
-				Date startDate = df.parse("2014-01-01");
-				Date endDate = cal.getTime();
-							
-				if(request.getParameter("date")!=null) {
-					String sdate = request.getParameter("date");
-					String edate = request.getParameter("date");
-					if(sdate.length()==4) {
-						sdate += "-01-01";
-						edate += "-12-31";
-					} else 
-					if(sdate.length()==7) {	
-						cal.set(Calendar.YEAR, Integer.parseInt(sdate.substring(0,4)));
-						cal.set(Calendar.MONTH, Integer.parseInt(sdate.substring(5,7))-1);
-						cal.set(Calendar.DATE, cal.getActualMaximum(Calendar.DATE));
-						sdate += "-01";
-						edate = df.format(cal.getTime());
-					}
-					startDate = df.parse(sdate);
-					endDate = df.parse(edate);				
-				}
-							
-				String dspaceURL = ConfigurationManager.getProperty("dspace.url");
-	
-				String urlParams =
-					  "&date=" + df.format(startDate) + "," + df.format(endDate)
-					+ "&period=" + period
-					+ "&idSite=" + PIWIK_SITE_ID
-					+ "&token_auth=" + PIWIK_AUTH_TOKEN
-					+ "&segment=pageUrl=@" + dspaceURL + "/handle/" + item.getHandle()
-					+ "&showColumns=label,url,nb_visits,nb_hits";
-				String downloadUrlParams =
-					  "&date=" + df.format(startDate) + "," + df.format(endDate)
-					+ "&period=" + period
-					+ "&idSite=" + PIWIK_DOWNLOAD_SITE_ID
-					+ "&token_auth=" + PIWIK_AUTH_TOKEN
-					+ "&segment=pageUrl=@" + dspaceURL + "/bitstream/handle/" + item.getHandle()
-					+ "&showColumns=label,url,nb_visits,nb_hits";
-	
-	
-				final boolean multi_requests = false;
-				queryString += "&token_auth=" + PIWIK_AUTH_TOKEN + "&module=API";
-				String piwikApiGetQuery = "method=Actions.getPageUrls&expanded=1&flat=1";
-	
-				if ( multi_requests ) {
-	
-					String report = PiwikHelper.readFromURL(
-						PIWIK_API_URL + rest + "?" + queryString + "&" + piwikApiGetQuery + urlParams
-					);
-					String downloadReport = PiwikHelper.readFromURL(
-						PIWIK_API_URL + rest + "?" + queryString + "&" + piwikApiGetQuery + downloadUrlParams
-					);
-					mergedResult = PiwikHelper.mergeJSON(report, downloadReport);
-				}else {
-					String piwikBulkApiGetQuery = "module=API&method=API.getBulkRequest&format=JSON"
-							+ "&token_auth=" + PIWIK_AUTH_TOKEN;
-	
-					
-					String url0 = URLEncoder.encode(piwikApiGetQuery + urlParams, "UTF-8");
-					String url1 = URLEncoder.encode(piwikApiGetQuery + downloadUrlParams, "UTF-8");
-					String report = PiwikHelper.readFromURL(
-						PIWIK_API_URL + rest + "?"
-							+ piwikBulkApiGetQuery
-							+ "&urls[0]=" + url0
-							+ "&urls[1]=" + url1
-					);
-					//mergedResult = PiwikHelper.mergeJSONResults(report);
-					mergedResult = PiwikHelper.transformJSONResults(report);
-					//mergedResult = report;
-	
-				}
-			} else 
-			if(PIWIK_API_MODE.equals("cached")) {
-				
-				String url = PIWIK_API_URL_CACHED + "handle?h=" + item.getHandle() + "&period=" + period;
-				
-				if(request.getParameter("date")!=null) {
-					String date = request.getParameter("date");
-					url += "&date=" + date;
-				}
-				
-				mergedResult = PiwikHelper.readFromURL(url);
-				
+				mergedResult = getDataFromPiwikServer();
+			} else if(PIWIK_API_MODE.equals("cached")) {
+				mergedResult = getDataFromLindatPiwikCacheServer();
 			}
-			
 			out.write(mergedResult.getBytes());
 			out.flush();
-			
 		} catch (Exception e) {
 			throw new ProcessingException("Unable to read piwik statisitcs", e);
 		}
-	}	
-	
+	}
+
+	private String getDataFromPiwikServer() throws Exception {
+		String viewsURL = buildViewsURL();
+		String downloadURL = buildDownloadsURL();
+		String bulkApiGetRequestURL = buildBulkApiGetRequestURL(viewsURL, downloadURL);
+
+		log.debug(String.format("Fetching data from piwik server; requesting \"%s\"", bulkApiGetRequestURL));
+
+		String report = PiwikHelper.readFromURL(bulkApiGetRequestURL);
+		return PiwikHelper.transformJSONResults(report);
+	}
+
+	private String buildBulkApiGetRequestURL(String... urls){
+		String piwikBulkApiGetQuery = "module=API&method=API.getBulkRequest&format=JSON"
+				+ "&token_auth=" + PIWIK_AUTH_TOKEN;
+		StringBuilder sb = new StringBuilder();
+		sb.append(PIWIK_API_URL)
+				.append(rest)
+				.append("?")
+				.append(piwikBulkApiGetQuery);
+		for(int i=0; i<urls.length; i++){
+			sb.append("&urls[")
+				.append(i)
+				.append("]=")
+				.append(urls[i]);
+		}
+		return sb.toString();
+	}
+
+	private String buildViewsURL() throws UnsupportedEncodingException, ParseException {
+		String dspaceURL = ConfigurationManager.getProperty("dspace.url");
+		String params =
+				"&idSite=" + PIWIK_SITE_ID
+				+ "&segment=pageUrl=@" + dspaceURL + "/handle/" + item.getHandle();
+		return buildURL(params);
+	}
+
+	private String buildDownloadsURL() throws UnsupportedEncodingException, ParseException {
+		String dspaceURL = ConfigurationManager.getProperty("dspace.url");
+		String params =
+				"&idSite=" + PIWIK_DOWNLOAD_SITE_ID
+				+ "&segment=pageUrl=@" + dspaceURL + "/bitstream/handle/" + item.getHandle();
+		return buildURL(params);
+	}
+
+	private String buildURL(String params) throws UnsupportedEncodingException, ParseException {
+		// should contain the period
+		String period = request.getParameter("period");
+		String dateRange = DateRange.fromDateString(request.getParameter("date")).toString();
+
+		/*
+		The Actions API lets you request reports for all your Visitor Actions: Page URLs, Page titles, Events,
+		Content Tracking, File Downloads and Clicks on external websites.
+		Actions.getPageUrls:
+		 - stats(nb_visits, nb_hits, etc) per url, in given date broken down by period
+		expanded:
+		- some API functions have a parameter 'expanded'. If 'expanded' is set to 1, the returned data will contain the first level results, as well as all sub-tables.
+		- basically fetches subtable (if present as idsubdatatable)
+		- eg. urls broken down by directory structure:
+			- lvl 1: <segment>pageUrl=^https%253A%252F%252Fdivezone.net%252Fdiving</segment>
+			- lvl 2: <segment>pageUrl==https%253A%252F%252Fdivezone.net%252Fdiving%252Fbali</segment>; <segment>pageUrl==https%253A%252F%252Fdivezone.net%252Fdiving%252Fthailand</segment>
+			- lvl 1 has no url, ie. it's cummulative for the "underlying" urls
+		flat:
+		- some API functions have a parameter 'expanded', which means that the data is hierarchical. For such API function, if 'flat' is set to 1, the returned data will contain the flattened view of the table data set. The children of all first level rows will be aggregated under one row. This is useful for example to see all Custom Variables names and values at once, for example, Matomo forum user status, or to see the full URLs not broken down by directory or structure.
+		- this will remove the cummulative results; all rows will be of the same type (having url field)
+		 */
+		String piwikApiGetQuery = "method=Actions.getPageUrls&expanded=1&flat=1";
+		String commonParams =
+				"&date=" + dateRange
+				+ "&period=" + period
+				+ "&token_auth=" + PIWIK_AUTH_TOKEN
+				+ "&showColumns=label,url,nb_visits,nb_hits";
+		return URLEncoder.encode(piwikApiGetQuery + commonParams + params, "UTF-8");
+	}
+
+	private String getDataFromLindatPiwikCacheServer() throws IOException {
+		String period = request.getParameter("period");
+		String url = PIWIK_API_URL_CACHED + "handle?h=" + item.getHandle() + "&period=" + period;
+
+		if(request.getParameter("date")!=null) {
+			String date = request.getParameter("date");
+			url += "&date=" + date;
+		}
+
+		return PiwikHelper.readFromURL(url);
+	}
+
+	private static class DateRange {
+		private static final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+		private final Date startDate;
+		private final Date endDate;
+
+		private DateRange(Date startDate, Date endDate){
+			this.startDate = startDate;
+			this.endDate = endDate;
+		}
+
+		private static DateRange fromDateString(String date) throws ParseException {
+			Date startDate;
+			Date endDate;
+			if(date!=null) {
+				String sdate = date;
+				String edate = date;
+				if(sdate.length()==4) {
+					sdate += "-01-01";
+					edate += "-12-31";
+				} else if(sdate.length()==7) {
+					Calendar cal = Calendar.getInstance();
+					cal.set(Calendar.YEAR, Integer.parseInt(sdate.substring(0,4)));
+					cal.set(Calendar.MONTH, Integer.parseInt(sdate.substring(5,7))-1);
+					cal.set(Calendar.DATE, cal.getActualMaximum(Calendar.DATE));
+					sdate += "-01";
+					edate = df.format(cal.getTime());
+				}
+				startDate = df.parse(sdate);
+				endDate = df.parse(edate);
+			} else {
+				// default start and end data
+				startDate = df.parse("2014-01-01");
+				endDate = Calendar.getInstance().getTime();
+			}
+			return new DateRange(startDate, endDate);
+		}
+
+		public String getFormattedStartDate(){
+			return df.format(startDate);
+		}
+
+		public String getFormattedEndDate(){
+			return df.format(endDate);
+		}
+
+		@Override
+		public String toString(){
+			return getFormattedStartDate() + "," + getFormattedEndDate();
+		}
+
+	}
+
 }
 
 
