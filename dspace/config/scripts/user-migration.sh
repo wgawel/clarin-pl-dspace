f1db986f30c082818355390c0d49dac65d645967
KamilT <kamilt@test>
2021-11-02 12:59:40 +0100

    disable migration scripts

--- dspace/config/scripts/user-migration.sh
+++ dspace/config/scripts/user-migration.sh
@@ -9,6 +9,6 @@ password=$2
 #Firstname + Lastname
 fullname=$3
 
-sh /opt/tomcat/webapps/dspace/bin/config/scripts/inforex-user-migration.sh $email $password $fullname
-sh /opt/tomcat/webapps/dspace/bin/config/scripts/wielowyr-user-migration.sh $email $password $fullname
-sh /opt/tomcat/webapps/dspace/bin/config/scripts/kuznia-user-migration.sh $email $password $fullname
+# sh /opt/tomcat/webapps/dspace/bin/config/scripts/inforex-user-migration.sh $email $password $fullname
+# sh /opt/tomcat/webapps/dspace/bin/config/scripts/wielowyr-user-migration.sh $email $password $fullname
+# sh /opt/tomcat/webapps/dspace/bin/config/scripts/kuznia-user-migration.sh $email $password $fullname
